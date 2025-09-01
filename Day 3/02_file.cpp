37880e9da8f4c8d80182f8c60c05c91aa8e53708
Harnoor <harnoorkaurdhiman@gmail.com>
2025-09-01 22:37:15 +0530

    Patterns

---
+++ Day 3/02_file.cpp
@@ -0,0 +1,18 @@
+// 10 10 10 10 10
+// 10 10 10 10 10
+// 10 10 10 10 10
+// 10 10 10 10 10
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << "10" << " ";
+        }
+        cout << endl;
+    }
+}
---
+++ Day 3/05_file.cpp
@@ -0,0 +1,18 @@
+// 5 4 3 2 1
+// 5 4 3 2 1
+// 5 4 3 2 1
+// 5 4 3 2 1
+// 5 4 3 2 1
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 5; i > 0; i--)
+    {
+        for (int j = 5; j > 0; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
